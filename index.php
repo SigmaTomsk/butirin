4c2aac8f39f208a333066dc8c1e8ec9c9c96c31e
AlekseyButyrin <abrahambaduga@gmail.com>
2019-05-13 11:28:43 +0700

    ver1

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php $title = "Главная | WebWiki";?>
+<html>
+
+	<head>
+		<link type="text/css" rel="stylesheet" href="CSS/main.css">
+		<title><?php echo $title;?></title>
+	</head>
+
+	<body link="#e2a81d" alink="#e26fab" vlink="#dc565c">
+		<center>
+			<div class="header"><img src="IMG/SERVER/logo.png" width="8%" height="80%" style="margin-top:1%; margin-left:10px;"><img src="IMG/SERVER/secondLogo.png" width="60%" height="80%" style="margin-top:1%; margin-left:10px;"></div>
+			<br><br><br><h1 style="font-family: Arial; font-size: 40px;">Главная!</h1></center>
+			<div>
+			<div style="float:left; width:17%; height:390%;background:#f40f40; margin:-15px; text-align:center;">
+			<br>
+			<a href="/HTML/g_comment.html">&lt;!-- --&gt;</a><br>
+			<a href="/HTML/g_a.html">&lt;a&gt;</a><br>
+			<a href="/HTML/g_abbr.html">&lt;abbr&gt;</a><br>
+			<a href="/HTML/g_acronym.html">&lt;acronym&gt;</a><br>
+			<a href="/HTML/g_adress.html">&lt;adress&gt;</a><br>
+			<a href="/HTML/g_applet.html">&lt;applet&gt;</a><br>
+			<a href="/HTML/g_area.html">&lt;area&gt;</a><br>
+			<a href="/HTML/g_article.html">&lt;article&gt;</a><br>
+			<a href="/HTML/g_aside.html">&lt;aside&gt;</a><br>
+			<a href="/HTML/g_audio.html">&lt;audio&gt;</a><br>
+			<a href="/HTML/g_b.html">&lt;b&gt;</a><br>
+			<a href="/HTML/g_base.html">&lt;base&gt;</a><br>
+			<a href="/HTML/g_basefont.html">&lt;basefont&gt;</a><br>
+			<a href="/HTML/g_bdi.html">&lt;bdi&gt;</a><br>
+			<a href="/HTML/g_bdo.html">&lt;bdo&gt;</a><br>
+			<a href="/HTML/g_bgsound.html">&lt;bgsound&gt;</a><br>
+			<a href="/HTML/g_blockquote.html">&lt;blockquote&gt;</a><br>
+			<a href="/HTML/g_big.html">&lt;big&gt;</a><br>
+			<a href="/HTML/g_body.html">&lt;body&gt;</a><br>
+			<a href="/HTML/g_blink.html">&lt;blink&gt;</a><br>
+			<a href="/HTML/g_br.html">&lt;br&gt;</a><br>
+			<a href="/HTML/g_button.html">&lt;button&gt;</a><br>
+			<a href="/HTML/g_canvas.html">&lt;canvas&gt;</a><br>
+			<a href="/HTML/g_caption.html">&lt;caption&gt;</a><br>
+			<a href="/HTML/g_center.html">&lt;center&gt;</a><br>
+			<a href="/HTML/g_cite.html">&lt;cite&gt;</a><br>
+			<a href="/HTML/g_code.html">&lt;code&gt;</a><br>
+			<a href="/HTML/g_col.html">&lt;col&gt;</a><br>
+			<a href="/HTML/g_colgroup.html">&lt;colgroup&gt;</a><br>
+			<a href="/HTML/g_command.html">&lt;command&gt;</a><br>
+			<a href="/HTML/g_commentt.html">&lt;comment&gt;</a><br>
+			<a href="/HTML/g_datalist.html">&lt;datalist&gt;</a><br>
+			<a href="/HTML/g_dd.html">&lt;dd&gt;</a><br>
+			<a href="/HTML/g_del.html">&lt;del&gt;</a><br>
+			<a href="/HTML/g_details.html">&lt;details&gt;</a><br>
+			<a href="/HTML/g_dfn.html">&lt;dfn&gt;</a><br>
+			<a href="/HTML/g_dir.html">&lt;dir&gt;</a><br>
+			<a href="/HTML/g_div.html">&lt;div&gt;</a><br>
+			<a href="/HTML/g_dl.html">&lt;dl&gt;</a><br>
+			<a href="/HTML/g_dt.html">&lt;dt&gt;</a><br>
+			<a href="/HTML/g_em.html">&lt;em&gt;</a><br>
+			<a href="/HTML/g_embed.html">&lt;embed&gt;</a><br>
+			<a href="/HTML/g_fieldset.html">&lt;fieldset&gt;</a><br>
+			<a href="/HTML/g_figcaption.html">&lt;figcaption&gt;</a><br>
+			<a href="/HTML/g_figure.html">&lt;figure&gt;</a><br>
+			<a href="/HTML/g_font.html">&lt;font&gt;</a><br>
+			<a href="/HTML/g_form.html">&lt;form&gt;</a><br>
+			<a href="/HTML/g_footer.html">&lt;footer&gt;</a><br>
+			<a href="/HTML/g_frame.html">&lt;frame&gt;</a><br>
+			<a href="/HTML/g_frameset.html">&lt;frameset&gt;</a><br>
+			<a href="/HTML/g_h1.html">&lt;h1&gt;</a><br>
+			<a href="/HTML/g_h2.html">&lt;h2&gt;</a><br>
+			<a href="/HTML/g_h3.html">&lt;h3&gt;</a><br>
+			<a href="/HTML/g_h4.html">&lt;h4&gt;</a><br>
+			<a href="/HTML/g_h5.html">&lt;h5&gt;</a><br>
+			<a href="/HTML/g_h6.html">&lt;h6&gt;</a><br>
+			<a href="/HTML/g_head.html">&lt;head&gt;</a><br>
+			<a href="/HTML/g_header.html">&lt;header&gt;</a><br>
+			<a href="/HTML/g_hgroup.html">&lt;hgroup&gt;</a><br>
+			<a href="/HTML/g_hr.html">&lt;hr&gt;</a><br>
+			<a href="/HTML/g_html.html">&lt;html&gt;</a><br>
+			<a href="/HTML/g_i.html">&lt;i&gt;</a><br>
+			<a href="/HTML/g_iframe.html">&lt;iframe&gt;</a><br>
+			<a href="/HTML/g_img.html">&lt;img&gt;</a><br>
+			<a href="/HTML/g_input.html">&lt;input&gt;</a><br>
+			<a href="/HTML/g_ins.html">&lt;ins&gt;</a><br>
+			<a href="/HTML/g_isindex.html">&lt;isindex&gt;</a><br>
+			<a href="/HTML/g_kbd.html">&lt;kbd&gt;</a><br>
+			<a href="/HTML/g_keygen.html">&lt;keygen&gt;</a><br>
+			<a href="/HTML/g_label.html">&lt;label&gt;</a><br>
+			<a href="/HTML/g_legend.html">&lt;legend&gt;</a><br>
+			<a href="/HTML/g_li.html">&lt;li&gt;</a><br>
+			<a href="/HTML/g_link.html">&lt;link&gt;</a><br>
+			<a href="/HTML/g_main.html">&lt;main&gt;</a><br>
+			<a href="/HTML/g_map.html">&lt;map&gt;</a><br>
+			<a href="/HTML/g_marquee.html">&lt;marquee&gt;</a><br>
+			<a href="/HTML/g_mark.html">&lt;mark&gt;</a><br>
+			<a href="/HTML/g_menu.html">&lt;menu&gt;</a><br>
+			<a href="/HTML/g_meta.html">&lt;meta&gt;</a><br>
+			<a href="/HTML/g_meter.html">&lt;meter&gt;</a><br>
+			<a href="/HTML/g_nav.html">&lt;nav&gt;</a><br>
+			<a href="/HTML/g_nobr.html">&lt;nobr&gt;</a><br>
+			<a href="/HTML/g_noembed.html">&lt;noembed&gt;</a><br>
+			<a href="/HTML/g_noframes.html">&lt;noframes&gt;</a><br>
+			<a href="/HTML/g_noscript.html">&lt;noscript&gt;</a><br>
+			<a href="/HTML/g_object.html">&lt;object&gt;</a><br>
+			<a href="/HTML/g_ol.html">&lt;ol&gt;</a><br>
+			<a href="/HTML/g_optgroup.html">&lt;optgroup&gt;</a><br>
+			<a href="/HTML/g_option.html">&lt;option&gt;</a><br>
+			<a href="/HTML/g_output.html">&lt;output&gt;</a><br>
+			<a href="/HTML/g_p.html">&lt;p&gt;</a><br>
+			<a href="/HTML/g_param.html">&lt;param&gt;</a><br>
+			<a href="/HTML/g_plaintext.html">&lt;plaintext&gt;</a><br>
+			<a href="/HTML/g_pre.html">&lt;pre&gt;</a><br>
+			<a href="/HTML/g_progress.html">&lt;progress&gt;</a><br>
+			<a href="/HTML/g_q.html">&lt;q&gt;</a><br>
+			<a href="/HTML/g_rp.html">&lt;rp&gt;</a><br>
+			<a href="/HTML/g_rt.html">&lt;rt&gt;</a><br>
+			<a href="/HTML/g_ruby.html">&lt;ruby&gt;</a><br>
+			<a href="/HTML/g_s.html">&lt;s&gt;</a><br>
+			<a href="/HTML/g_samp.html">&lt;samp&gt;</a><br>
+			<a href="/HTML/g_script.html">&lt;script&gt;</a><br>
+			<a href="/HTML/g_section.html">&lt;section&gt;</a><br>
+			<a href="/HTML/g_select.html">&lt;select&gt;</a><br>
+			<a href="/HTML/g_small.html">&lt;small&gt;</a><br>
+			<a href="/HTML/g_span.html">&lt;span&gt;</a><br>
+			<a href="/HTML/g_source.html">&lt;source&gt;</a><br>
+			<a href="/HTML/g_strike.html">&lt;strike&gt;</a><br>
+			<a href="/HTML/g_strong.html">&lt;strong&gt;</a><br>
+			<a href="/HTML/g_style.html">&lt;style&gt;</a><br>
+			<a href="/HTML/g_sub.html">&lt;sub&gt;</a><br>
+			<a href="/HTML/g_summary.html">&lt;summary&gt;</a><br>
+			<a href="/HTML/g_sup.html">&lt;sup&gt;</a><br>
+			<a href="/HTML/g_table.html">&lt;table&gt;</a><br>
+			<a href="/HTML/g_tbody.html">&lt;tbody&gt;</a><br>
+			<a href="/HTML/g_td.html">&lt;td&gt;</a><br>
+			<a href="/HTML/g_textarea.html">&lt;textarea&gt;</a><br>
+			<a href="/HTML/g_tfoot.html">&lt;tfoot&gt;</a><br>
+			<a href="/HTML/g_th.html">&lt;th&gt;</a><br>
+			<a href="/HTML/g_thead.html">&lt;thead&gt;</a><br>
+			<a href="/HTML/g_time.html">&lt;time&gt;</a><br>
+			<a href="/HTML/g_title.html">&lt;title&gt;</a><br>
+			<a href="/HTML/g_tr.html">&lt;tr&gt;</a><br>
+			<a href="/HTML/g_tt.html">&lt;tt&gt;</a><br>
+			<a href="/HTML/g_u.html">&lt;u&gt;</a><br>
+			<a href="/HTML/g_ul.html">&lt;ul&gt;</a><br>
+			<a href="/HTML/g_var.html">&lt;var&gt;</a><br>
+			<a href="/HTML/g_video.html">&lt;video&gt;</a><br>
+			<a href="/HTML/g_wbr.html">&lt;wbr&gt;</a><br>
+			<a href="/HTML/g_xmp.html">&lt;xmp&gt;</a><br>
+			</div><div><center>
+			<h4 style="color:#00003f; margin-left: -100px">Приветствуем вас на нашем сайте! Он сделан для тех, кто <br>хочет повторить язык программирования html, или узнать его основы.</h4>
+		</center></div>
+		<div style="width:100%; height:20%; background:#dc565c; margin-top:185%">
+		<p>Ля-ля-ля, ля-ля-ля, все права не защищены (c)</p>
+		</div>
+	</body>
+</html>
